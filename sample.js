f12c4addfc2141418aae0f2c42aa1c86a03f4176
Surya <suryaunder18@gmail.com>
2026-05-06 23:53:49 +0530

    learning express

--- sample.js
+++ sample.js
@@ -4,3 +4,13 @@ async function sample(){
     console.log(data);
 }
 
+
+const express = require('express');
+const app =express();
+
+app.get("/",(req,res) => {
+    res.send('Hello World');
+})
+app.listen(3000,() => {
+    console.log('Server is running on port 3000');
+})
